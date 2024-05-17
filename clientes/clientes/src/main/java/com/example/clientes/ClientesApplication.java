bdbf34170ea2cbf97e7c473faf39d5b185ecb42c
wallace301089 <115509492+wallace301089@users.noreply.github.com>
2024-05-17 16:31:11 -0300

    dado inicio a classe clieente do projeto do salao de beleza

---
+++ clientes/clientes/src/main/java/com/example/clientes/ClientesApplication.java
@@ -0,0 +1,14 @@
+package com.example.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApplication.class, args);
+		System.out.println("alo mundo");
+	}
+
+}
