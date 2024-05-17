eae30cc9deee6fe42f7c656827d3c89195ee9d1d
Lucas Almeida <lucas.a1meida@hotmail.com>
2024-05-17 17:29:25 -0300

    teste de commit

--- clientes/clientes/src/main/java/com/example/clientes/ClientesApplication.java
+++ clientes/clientes/src/main/java/com/example/clientes/ClientesApplication.java
@@ -8,7 +8,7 @@ public class ClientesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
-		System.out.println("alo mundo");
+		System.out.println("alo mundo, wallace vai ser o melhor programador da Outedg um dia");
 	}
 
 }
